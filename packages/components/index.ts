600948a927d0e9e629f67d8343060cb517301d60
Lian Xun <xun@lavax.co>
2021-03-05 12:29:35 +0800

    upgrade next transpile

---
+++ packages/components/index.ts
@@ -0,0 +1,4 @@
+// https://github.com/martpie/next-transpile-modules/releases/tag/6.0.0
+// must have a index.ts file otherwise the next-transpile will not work
+
+export * from "./src";
--- packages/web-nextjs/next.config.js
+++ packages/web-nextjs/next.config.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-param-reassign,@typescript-eslint/no-var-requires */
+const path = require('path');
 
-const withTM = require('next-transpile-modules')(['components'], {
-  resolveSymlinks: true,
+// https://github.com/martpie/next-transpile-modules/issues/101
+const withTM = require('next-transpile-modules')([
+    'components'
+  ], {
+  debug: true
 })
 
 module.exports = withTM({
-  webpack(config) {
+  webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
