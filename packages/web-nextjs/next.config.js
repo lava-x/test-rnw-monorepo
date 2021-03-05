277a10dac9448c1df9737f84e7f29a2337378d1e
Lian Xun <xun@lavax.co>
2021-03-05 10:38:30 +0800

    initial setup

--- packages/web-nextjs/next.config.js
+++ packages/web-nextjs/next.config.js
@@ -8,6 +8,7 @@ module.exports = withTM({
   webpack(config) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web',
     }
     config.resolve.extensions = [
